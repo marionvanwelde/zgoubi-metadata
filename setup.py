06b50a689480099dd8c5f0481e33f3533b568ffa
Sam Tygier <sam.tygier@manchester.ac.uk>
2020-03-17 16:29:45 +0000

    Add elements yaml and basic package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+
+setup(
+	name="Zgoubi metadata",
+	version="0.1",
+	packages=find_packages(),
+	include_package_data=True,
+)
