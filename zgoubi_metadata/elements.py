8aa33189afe9bfb2eefb946fea7a9e728b365e3b
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2020-07-11 14:00:29 +0200

    Minor fixes.
    Signed-off-by: Cédric Hernalsteens <cedric.hernalsteens@ulb.be>

--- zgoubi_metadata/elements.py
+++ zgoubi_metadata/elements.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
-
 import os
 import pkg_resources
 
 _directory = "data/elements_yaml/"
+
 
 def get_raw_yaml():
 	elements_yaml = {}
@@ -11,10 +10,11 @@ def get_raw_yaml():
 		elements_yaml[_f.rpartition(".")[0]] = pkg_resources.resource_string(__name__, os.path.join(_directory, _f))
 	return elements_yaml
 
+
 def get_parsed():
 	from yaml import safe_load
 
 	elements = {}
-	for k,v in get_raw_yaml().items():
+	for k, v in get_raw_yaml().items():
 		elements[k] = safe_load(v)
 	return elements
